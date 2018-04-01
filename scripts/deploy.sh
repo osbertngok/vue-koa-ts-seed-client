d3eb0f3c59f8f170a6c6221d61aeeb566f26210f
Osbert Ngok <osbertngok@gmail.com>
2018-04-01 23:56:24 +0800

    minor fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+
+CURR_FILE_DIR=`dirname $0`
+PROJECT_DIR=${CURR_FILE_DIR}/..
+
+DEST=root@test-sgp-01
+
 # poor man's docker hub
 # prefer to use hub if needs to be done multiple times, as docker pull can utilize layers that are already pulled
-docker save vue_koa_ts_seed_client:latest | bzip2 | pv | ssh root@test-sgp-01 'bunzip2 | docker load'
-docker save vue_koa_ts_seed_server:latest | bzip2 | pv | ssh root@test-sgp-01 'bunzip2 | docker load'
+docker save vue_koa_ts_seed_client:latest | bzip2 | pv | ssh ${DEST} 'bunzip2 | docker load'
+docker save vue_koa_ts_seed_server:latest | bzip2 | pv | ssh ${DEST} 'bunzip2 | docker load'
 
-# the following is better to be handled with ansible, but just for simplicity:
-
+ssh ${DEST} 'mkdir -p /opt/main/ops_web'
+scp ${PROJECT_DIR}/docker/docker-compose.yml ${DEST}:/opt/main/ops_web/
+scp ${PROJECT_DIR}/docker/CaddyFile ${DEST}:/opt/main/ops_web/
+ssh ${DEST} "sudo docker-compose -f /opt/main/ops_web/docker-compose.yml up --force-recreate -d"
