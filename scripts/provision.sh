b82582f383adf06ea6a62b39a8adcabdb7a0eeb6
Osbert Ngok <osbertngok@gmail.com>
2018-04-01 23:32:26 +0800

    add provision folders

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -4,3 +4,4 @@ CURR_FILE_DIR=`dirname $0`
 PROJECT_DIR=${CURR_FILE_DIR}/..
 
 ansible-playbook -i ${PROJECT_DIR}/ansible/inventory.yml -e "ansible_ssh_user=root" ${PROJECT_DIR}/ansible/provision.yml --limit ops_web
+
